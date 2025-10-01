77ccbd03b8f9416393ca635238f6aaf11165315b
Ayla Fazi <nisafazilatunalya@gmail.com>
2025-10-01 14:20:33 +0700

    percobaan 3

---
+++ jobsheet5/nestedUjianSkripsi06.java
@@ -0,0 +1,32 @@
+package jobsheet5;
+import java.nio.channels.Pipe.SourceChannel;
+import java.util.Scanner;
+public class nestedUjianSkripsi06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+         System.out.println("Apakah mahasiswa sudah bebas kompen? (Ya/Tidak): ");
+         String bebasKompen = sc.nextLine(). trim();
+
+         System.out.println("Masukkan jumlah log bimbingan Pembimbing 1: ");
+         int bimbinganP1 = sc.nextInt();
+         System.out.println("Masukkan jumlah log bimbingan Pembimbing 2: ");
+         int bimbinganP2 = sc.nextInt();
+
+    if (bebasKompen.equalsIgnoreCase("Ya")) {
+        if (bimbinganP1 >= 8 && bimbinganP2 >= 4) {
+            System.out.println("Semua syarat terpenuhi. Mahasiswa boleh mendaftar ujian skripsi");
+        }else if (bimbinganP1 <8 && bimbinganP2 < 4) {
+            System.out.println("Gagal! Log bimbingan P1 kurang dari 8 kali dan P2 kurang dari 4 kali");
+        } else if (bimbinganP1 < 8) {
+            System.out.println("Gagal! Log bimbingan P1 belum mencapai 8 kali");
+        }
+            
+        }else {
+            System.out.println("Gagal! Mahasiswa masih memiliki tangguan kompen");
+        }
+        System.out.println("pesan");
+        sc.close();
+    }
+    }
+
+
